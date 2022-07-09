a18202a3c85455a72cd1006128737af9567bfa51
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-09 20:58:44 +0200

    Completed.

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const timeLeft = document.querySelector("#timeLeft");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
+const timeBetweenMove = 1000;
+let currentTime = 60;
+let timerID = null;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,11 +16,35 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
 
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
 function moveMole() {
-  let timerID = null;
-  timerID = setInterval(randomSquare, 500);
+  timerID = setInterval(randomSquare, timeBetweenMove);
 }
 
 moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerID);
+    clearInterval(timerID);
+    alert("Game Over. Your score is: " + result);
+  }
+}
+
+let countDownTimerID = setInterval(countDown, 1000);
